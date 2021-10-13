c246f9a2c9fcc54c07ab6d1e20534ca49fca13d9
TuanNQT <63988154+TuanNQT@users.noreply.github.com>
2021-10-13 11:43:51 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
       <jw-pagination
         :items="listallitem"
         @changePage="onChangePage"
-        :pageSize=10
+        :pageSize="10"
         :maxPages="10"
       ></jw-pagination>
     </div>
@@ -34,7 +34,7 @@ Vue.use(VueAxios, axios);
 export default {
   name: "CommentList",
   data() {
-    return {listallitem:[], pageOfItems: [] };
+    return { listallitem: [], pageOfItems: [] };
   },
   mounted() {
     Vue.axios
@@ -79,3 +79,116 @@ li.inline {
   display: inline;
 }
 </style>
+<!--  thủ công
+<template>
+<div class="col-sm-12">
+  <div class="offset">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th> Name</th>
+        <th>Email Name</th>
+        <th>Body</th>
+      </tr>
+    </thead>
+    <tbody>
+    <tr v-for="p in displayedPosts" v-bind:key="p.id">
+      <td>{{p.name}}</td>
+      <td>{{p.email}}</td>
+      <td>{{p.body}}</td>
+    </tr>
+    </tbody>
+  </table>
+    <nav aria-label="Page navigation example">
+			<ul class="pagination">
+				<li class="page-item">
+					<button type="button" class="page-link" v-if="page != 1" @click="page--"> Previous </button>
+				</li>
+				<li class="page-item">
+					<button type="button" class="page-link" v-for="pageNumber in pages.slice(page-1, page+5)" v-bind:key="pageNumber.index" @click="page = pageNumber"> {{pageNumber}} </button>
+				</li>
+				<li class="page-item">
+					<button type="button" @click="page++" v-if="page < pages.length" class="page-link"> Next </button>
+				</li>
+			</ul>
+		</nav>	
+  </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import Vue from "vue";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+export default {
+  name :"CommentList",
+  data() {
+    return {
+      posts: [""],
+      page: 1,
+      perPage: 9,
+      pages: [],
+    };
+  },
+  methods: {
+    getPosts() {
+      axios.get("http://jsonplaceholder.typicode.com/comments").then((resp) => {
+        this.posts = resp.data;
+        console.log(resp.data);
+      });
+    },
+    setPages() {
+      let numberOfPages = Math.ceil(this.posts.length / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
+    },
+    paginate(posts) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return posts.slice(from, to);
+    },
+  },
+  computed: {
+    displayedPosts() {
+      return this.paginate(this.posts);
+    },
+  },
+  watch: {
+    posts() {
+      this.setPages();
+    },
+  },
+  created() {
+    this.getPosts();
+  },
+  filters: {
+    trimWords(value) {
+      return value.split(" ").splice(0, 20).join(" ") + "...";
+    },
+  },
+};
+</script>
+<style scoped>
+li.page-item{
+  display: inherit;
+}
+button.page-link {
+	display: inline-block;
+}
+button.page-link {
+    font-size: 20px;
+    color: #29b3ed;
+    font-weight: 500;
+}
+.offset{
+  width: 800px !important;
+  margin: 20px auto;  
+}
+</style> 
+-->
+
+
